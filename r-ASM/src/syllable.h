36a820432ce56021570df68ecade618ee3ed7d02
Thijs van As <tvanas@users.noreply.github.com>
2008-07-15 19:41:20 +0000

    * quickstart for XUP V2P board added * licensing information added

--- r-ASM/src/syllable.h
+++ r-ASM/src/syllable.h
@@ -5,6 +5,25 @@
  *
  * Copyright (c) 2008, Thijs van As <t.vanas@gmail.com>
  *
+ * Computer Engineering Laboratory
+ * Delft University of Technology
+ * Delft, The Netherlands
+ * 
+ * http://r-vex.googlecode.com
+ * 
+ * r-ASM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  * syllable.h
  */
 
