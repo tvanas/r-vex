2e1ecfecd46174b5e47bd6ea0c1b8ccb5a3f3bd9
Thijs van As <tvanas@users.noreply.github.com>
2008-07-19 13:35:50 +0000

    * reference outputs for demos added

--- demos/dummy.s
+++ demos/dummy.s
@@ -1,3 +1,11 @@
+# Dummy demo
+# -----------------------
+#
+# Data memory contents after running:
+# 0x00: 0xFF
+# 0x01: 0x0F
+# 0x02: 0xEF1
+
 add $r0.15 = $r0.0, 1
 mov $r0.1 = $r0.0
 mov $r0.2 = $r0.0
@@ -6,7 +14,7 @@ add $r0.1 = $r0.0, 255
 ;;
 add $r0.2 = $r0.0, 15
 ;;
-mpyll $r0.3 = $r0.1, $r0.2
+mpyll $r0.3 = $r0.1, $r0.2 # $r0.3 = $r0.1 * $r0.2
 ;;
 stw 0x0[$r0.15] = $r0.1
 ;;
